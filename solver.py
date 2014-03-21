4cd7b7922c7c044d384472cd5389532450d7eabd
andrejborstnik <andrej-borstnik@hotmail.com>
2014-03-21 13:44:41 +0100

    Še naprej pišem chaff sat solver.

--- solver.py
+++ solver.py
@@ -153,55 +153,141 @@ def DPLLpomo(formula,vrednosti = {}):
 
 
 def Chaff(formula):
-    formula = formula.kopiraj().poenostavi(True)
+    def slovar(vrednosti,kazalci):
+        pomo = {}
+        for i in range(len(vrednosti)):
+            pomo[kazalci[i]] = vrednosti[i][0]
+        return pomo
+    def izberi(literali):
+        a = literali[0]
+        j = 0
+        for i in range(len(literali)):
+            if literali[i] > a:
+                a = literali[i]
+                j = i
+        return j
+    formula = formula.poenostavi(True)
     spr = formula.spremenljivke()
     kazalci = [0] * len(spr)
     obratnikazalci = {}
     opazovaneSpr={}#Slovar, ki za spr. pove v katerih stavkih je opazovana.
+    sprStavki = {}#Slovar, ki za spr. pove v katerih stavkih je vsebovana.
     k=0
     for i in spr:
         kazalci[k] = i
         obratnikazalci[i] = k
-        opazovane[i] = set()
+        opazovaneSpr[i] = set()
         k += 1
-    vrednosti = [None] * len(kazalci)
+    vrednosti = [None] * len(spr)
+    literali = [0] * (2*len(spr))
+    koraki = Sklad()
+
+    #dokler imamo enojce jih krajšamo.
+    novevrednosti = {0:1}
+    while novevrednosti:
+        novevrednosti = {}
+        odstrani = []
+        for i in formula.sez:
+            #In(Ali(Spr("x"))) je že poenostavljen v In(Spr("x")), In(Ali()) pa v False, tako da smo pokrili vse primere.
+            if type(i) == Spr:
+                novevrednosti[i.ime] = True
+                vrednosti[obratnikazalci[i.ime]] = (True,2)
+                odstrani.append(i)
+            elif type(i) == Neg:#Negacija ima notri samo eno spr, saj smo že poenostavili.
+                odstrani.append(i)
+                novevrednosti[i.izr.ime] = False
+                vrednosti[obratnikazalci[i.izr.ime]] = (False,2)
+            for i in odstrani:
+                formula.sez.remove(i)
+            formula = formula.vstavi(novevrednosti).poenostavi(True)
+            if type(formula)==T:
+                return slovar(vrednosti)
+            elif type(formula)==F:
+                return False
+            elif type(formula) == Spr:
+                vrednosti[obratnikazalci[formula.ime]] = (True,2)
+                return slovar(vrednosti)
+            elif type(formula) == Neg:
+                vrednosti[obratnikazalci[formula.izr.ime]] = (False,2)
+                return slovar(vrednosti)
+    
     izraz = []#Formulo spremenimo v seznam seznamov, ker bo tako lažje izvajati algoritem.
-    opazovaneSta = [] #Seznam, ki za stavke pove kateri spremenljivki sta v njem opazovani.
+    opazovaneSta = [] #Seznam, ki za stavke pove kateri spremenljivki sta v njem opazovani (lahko tudi ena ali 0 spr).
     for i in formula.sez:
         podizraz = []
-        """??? To bomo naredili prej. Izvedli bomo while zanko kot pri DPLL, nato pa nadaljevali po chaff-u. V vsakem stavku rabimo namreč vsaj dve spremenljivki."""
         if len(i.sez)>1:
-##            for j in i.sez:
-##                if type(j) == Spr: 
-##                    podizraz.append((obratnikazalci[j],False)) #False pomeni da spremenljivka ni negirana
-##                elif type(j) == Neg:
-##                    podizraz.append((obratnikazalci[j.izr],True))
-##                else:
-##                    raise UsageError("Ta formula ni bila poenostavljena pravilno.")
-##                
-##            izraz.append(podizraz)
-##            opazene = 0
-##            k = 0
-##            while opazene < 2:
-##                pomo = podizraz[k]
-            pass
-                
-                
+            for j in i.sez:
+                if type(j) == Spr:
+                    podizraz.append((obratnikazalci[j],False)) #False pomeni da spremenljivka ni negirana
+                    if obratnikazalci[j] in sprStavki.keys():
+                        sprStavki[obratnikazalci[j]].add(len(izraz)-1)
+                    else:
+                        sprStavki[obratnikazalci[j]]={len(izraz)-1}
+                elif type(j) == Neg:
+                    podizraz.append((obratnikazalci[j.izr],True))
+                    if obratnikazalci[j.izr] in sprStavki.keys():
+                        sprStavki[obratnikazalci[j.izr]].add(len(izraz)-1)
+                    else:
+                        sprStavki[obratnikazalci[j.izr]]={len(izraz)-1}
+                else:
+                    raise UsageError("Ta formula ni bila poenostavljena pravilno.")
+            izraz.append(podizraz)
+            opazovaneSta.append([podizraz[0],podizraz[1]])
+            if podizraz[0] in opazovaneSpr.keys():
+                opazovaneSpr[podizraz[0]].add(len(izraz)-1)
+            else:
+                opazovaneSpr[podizraz[0]] = {len(izraz)-1}
+            if podizraz[1] in opazovaneSpr.keys():
+                opazovaneSpr[podizraz[1]].add(len(izraz)-1)
+            else:
+                opazovaneSpr[podizraz[1]] = {len(izraz)-1}
+        else:
+            raise UsageError("To se ne bi smelo zgoditi, saj smo vse enojce krajšali.")
+        
+    while ...:
+        b = izberi(literali)#izbere literal, ki ima največjo vrednost po cca. chaff metodi
+        if b >= len(spr):
+            if vrednosti[b-len(spr)] and vrednosti[b-len(spr)][1] == 2:
+                return False
+            elif vrednosti[b-len(spr)]:
+                vrednosti[b-len(spr)] = (False,2)
+            else:
+                vrednosti[b-len(spr)] = (False,1)
+        else:
+            if vrednosti[b] and vrednosti[b][1] == 2:
+                return False
+            elif vrednosti[b]:
+                vrednosti[b] = (True,2)
+            else:
+                vrednosti[b] = (True,1)
+        for i in opazovaneSpr.get(b,[]):
+            #najde novo spremenljivko, ki jo opazujemo v i, če primerna obstaja.
+            opazovaneSta[i].remove(b)
+            pommo = True
+            for j in izraz[i]:
+                if vrednosti[j] and vrednosti[j][1]:
+                    opazovaneSta[i].append(b)
+                    if b in opazovaneSpr.keys():
+                        opazovaneSpr[b].add(i)
+                    else:
+                        opazovaneSpr[b] = {i}
+                    pommo = False
+                    break
+            if pommo:#Nismo našli nove spr, torej mora biti spr, ki je ostala true. Shranimo si tudi spr., ki je false, saj se bo pri backtrackingu spremenila na true
+                #vemo, da mora biti zadnji literal true
+                if vrednosti[opazovaneSta[i][0]] and vrednosti[opazovaneSta[i][0]][1] == 2 and vrednosti[opazovaneSta[i][0]][0] == False:
+                    return False
+                elif vrednosti[opazovaneSta[i][0]] and vrednosti[opazovaneSta[i][0]][0] == False:
+                    vrednosti[opazovaneSta[i][0]] = (True,2)
+                else:
+                    pass#mogoče bom tu dodal kakšen assert.
+                opazovaneSta[i].append(b)
                     
                 
-        else:
-            if type(i) == Spr:
-                vrednosti[obratnikazalci[i]] = (True,2)#2, ker so to dokončne vrednosti in če bi se morale spremeniti bomo vrnili, da formula ni izponljiva
-            elif type(i) == Neg:
-                vrednosti[obratnikazalci[i.izr]] = (False,2)
-            else:
-                raise UsageError("Ta formula ni bila poenostavljena pravilno.")
         
+            
+
+
     return None
-
-def pomoChaff(izraz):
-    return None
-
-
 
 
